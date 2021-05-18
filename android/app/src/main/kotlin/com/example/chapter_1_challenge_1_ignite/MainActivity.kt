494dae0ddc334da5616b0ac531e99a47a4a44a26
Gabriel Nicol <moraesnicol@gmail.com>
2021-05-17 22:53:03 -0300

    init

---
+++ android/app/src/main/kotlin/com/example/chapter_1_challenge_1_ignite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chapter_1_challenge_1_ignite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MeuWidget());
+}
+
+class MeuWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
