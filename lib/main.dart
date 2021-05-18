3d2d0f7f90062748ba3ea656043a94b59b8f4550
Gabriel Nicol <moraesnicol@gmail.com>
2021-05-18 00:15:25 -0300

    themedata added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'core/pages/home_page.dart';
+
 void main() {
-  runApp(MeuWidget());
+  runApp(AppWidget());
 }
 
-class MeuWidget extends StatelessWidget {
+class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My first App",
+      home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+          primaryColor: Colors.pink,
+          scaffoldBackgroundColor: Colors.amber,
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+              backgroundColor: Colors.purpleAccent)),
+      darkTheme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.grey,
+          floatingActionButtonTheme:
+              FloatingActionButtonThemeData(backgroundColor: Colors.black87)),
     );
   }
 }
